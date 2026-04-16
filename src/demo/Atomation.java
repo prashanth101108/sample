affb5a6df1deb32dc62702c97d8015ab6ee27024
2035 <2035@DEV-074.oasys.com>
2026-04-16 22:00:07 +0530

    1st

---
+++ src/demo/Atomation.java
@@ -0,0 +1,12 @@
+package demo;
+
+public class Atomation {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Automation testing...");
+
+	}
+
+}
